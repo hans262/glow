602e502aa1f75835cfc103585dd3acf88e2b244e
hans <771565119@qq.com>
2021-12-28 16:48:26 +0800

    dqqw

--- src/styled-components/Button.tsx
+++ src/styled-components/Button.tsx
@@ -1,13 +1,12 @@
-import styled from "styled-components";
+import React from 'react'
 
-export const Button = styled.button<{
-  hoverColor?: string
-}>`
-  cursor: pointer;
-  :hover {
-    color: ${props => props.hoverColor ? props.hoverColor : 'white'};
-    background: #444;
-  }
-  border: none;
-  margin-right: 5px;
-`
+export const Button: React.FC = ({ children }) => {
+  return (
+    <button className={
+      'text-white py-3 px-6 shadow rounded-full cursor-pointer hover:bg-hover ' +
+      'hover:text-white font-bold md:text-lg bg-blue-700'
+    }>
+      {children}
+    </button>
+  )
+}
--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -1,9 +1,11 @@
-import MySwiper from "../common/MySwiper";
+// import MySwiper from "../common/MySwiper";
+
+import { Button } from "../styled-components/Button";
 
 export default function Test() {
   return (
-    <div>
-      <MySwiper />
+    <div style={{ padding: '20px' }}>
+      <Button>按钮</Button>
     </div>
   )
 }
